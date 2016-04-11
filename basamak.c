5b5f437824b30c12f910d41a0aed0e2892b29aeb
Mustafa <msanli14@posta.pau.edu.tr>
2016-04-11 04:18:53 +0300

    Create basamak.c
    sayiları basamak degerlerine ayırıp her sayının degerinin karesini topluyor ve sonunda bir döngüye girip girmedigini kontrol ediyor örnegin 120=1*1+2*2+0*0 olarak

---
+++ basamak.c
@@ -0,0 +1,59 @@
+// MUSTAFA SANLI
+// 14253055    
+#include<stdio.h>
+#include <stdlib.h>
+void main()
+{
+	bitti:
+	system("color 9f");
+	int y, x, z, m = 0;
+	char a = "mustafa",b;
+	printf("Bir Sayi Girin: ");
+	scanf_s("%d", &x);//alınan sayi x
+	printf("Girdiginiz Sayi %d Islem Basliyor...\n", x);
+	_getch();
+	
+	while (a = "mustafa")
+	{
+		while (x != 0)
+		{
+			y = x % 10;  
+			x = x / 10;
+			z = y*y;
+			m += z;
+		}
+		x = m;
+		printf("Sonuc: %d\n", m);
+		_getch();
+		switch (m)
+		{
+		case 89:
+			printf("89 ile dongu basliyor\n");
+			goto bitti;
+			break;
+        case 16:
+			printf("16 ile dongu basliyor\n");
+			goto bitti;
+			break;
+		case 145:
+			printf("145 ile dongu basliyor\n");
+			goto bitti;
+			break;
+		case 1:
+			printf("1 ile dongu basliyor\n");
+			goto bitti;
+			break;
+		case 58:
+			printf("58 ile dongu basliyor\n");
+			goto bitti;
+			break;
+		}
+	m = 0;
+		}
+}
+	
+	
+
+		
+
+
